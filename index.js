5d8f5beafc0d6bc516f31ea0cb399ebd5003ed8a
rafaballerini <rafaella_ballerini@hotmail.com>
2021-06-26 19:15:00 -0300

    codefi

---
+++ index.js
@@ -0,0 +1,46 @@
+import 'dotenv/config'
+import Discord from 'discord.js'
+import YTDL from 'ytdl-core'
+
+const url = '5qap5aO4i9A'
+const channelId = '836004917973614666'
+let playing = false
+
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"]
+})
+
+function play(connection) {
+  playing = true
+  const stream = YTDL(url, {filter: "audioonly"})
+  connection.play(stream, {volume: 1, seek: 0})
+}
+
+async function replay() {
+  const voiceChannel = await client.channels.fetch(channelId)
+  if (voiceChannel) {
+    voiceChannel.join().then(connection => {
+      if (!playing){
+        play(connection)
+      }
+    }).catch(error => {
+      console.log(error)
+      playing = false
+    })
+  }
+}
+
+client.on('ready', async() => {
+  console.log("Codefi on")
+  replay()
+})
+
+client.on('message', message => {
+  if (message.content === '<3play'){
+    if (!playing) {
+      replay()
+    }
+  }
+})
+
+client.login(process.env.token)
