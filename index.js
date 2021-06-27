3163762cf1f3b972bebaa0c9a7d49775b302da9a
Mateus Andriola <mateusandriola18@gmail.com>
2021-06-27 17:26:52 -0300

    Refactor

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
-import "dotenv/config";
-import Discord from "discord.js";
-import YTDL from "ytdl-core";
+import 'dotenv/config'
+import Discord from 'discord.js'
+import YTDL from 'ytdl-core'
 
-const url = "https://www.youtube.com/watch?v=_DYAnU3H7RI";
-const channelId = "858420360441364500";
-let playing = false;
+const url = 'https://www.youtube.com/watch?v=_DYAnU3H7RI'
+const channelId = '858420360441364500';
+let playing = false
 
 const client = new Discord.Client({
-  partials: ["MESSAGE", "CHANNEL", "REACTION"],
-});
+  partials: ["MESSAGE", "CHANNEL", "REACTION"]
+})
 
-async function play(connection) {
-  playing = true;
-  const stream = YTDL(url, { filter: "audio" });
-  const DJ = connection.play(stream, { seek: 0, volume: 0.09 });
-  DJ.on("finish", async (end) => {
-    await play(connection);
-  });
+const play = async (connection) =>
+  new Promise((resolve, reject) => {
+    playing = true;
+    const stream = YTDL(url, { filter: 'audioonly' });
+    const dj = connection.play(stream, { seek: 0, volume: 1.5 });
+    dj.on('finish', () => {
+      playing = false;
+      resolve()
+    })
+    dj.on('error', () => {
+      playing = false;
+      connection.disconnect()
+      reject()
+    })
+    dj.on('unpipe', () => {
+      playing = false;
+      reject()
+    })
+  })
+
+const connect = async () => {
+  const voiceChannel = await client.channels.fetch(channelId);
+  if (!voiceChannel) {
+    throw new Error("Canal não encontrado");
+  }
+  return await voiceChannel.join();
 }
 
 async function replay() {
-  const voiceChannel = await client.channels.fetch(channelId);
-  if (voiceChannel) {
-    voiceChannel
-      .join()
-      .then((connection) => {
-        if (!playing) {
-          play(connection);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        playing = false;
-      });
+  const voiceConnection = await connect();
+  try {
+    while (!playing) {
+      await play(voiceConnection);
+    }
+  } finally {
+    playing = false;
+    voiceConnection.disconnect()
   }
 }
 
-client.on("ready", async () => {
-  console.log("Codefi on");
-  replay();
-});
+client.on('ready', async () => {
+  console.log("Codefi on")
+  await replay()
+})
 
-client.on("message", (message) => {
-  if (message.content === "<3play") {
+client.on('message', async message => {
+  if (message.content === '<3replay') {
     if (!playing) {
-      replay();
+      await replay()
     }
   }
-});
+})
 
-client.login(process.env.token);
+client.login(process.env.token)
