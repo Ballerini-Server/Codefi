6f7814bd66bcd573802e8493b4ded472af01c254
José Gustavo <josegustavo25565@gmail.com>
2021-06-30 18:03:03 -0300

    Add Ready event file

---
+++ src/events/Ready.js
@@ -0,0 +1,40 @@
+import BaseEvent from "./BaseEvent";
+import { createBroadcast, play } from "../utils/play";
+
+const { channelId } = process.env;
+
+export default class Ready extends BaseEvent {
+	constructor() {
+		super("ready");
+	}
+
+	async run(client) {
+		client.user.setPresence({
+			activity: {
+				name: "Coding with Lo-fi",
+				type: "STREAMING",
+				url: "https://www.twitch.tv/rafaballerinii",
+			},
+			status: "dnd",
+		});
+
+		let channel =
+			client.channels.cache.get(channelId) ||
+			(await client.channels.fetch(channelId));
+
+		if (!channel) {
+			console.error("Canal não existe");
+			return process.exit(1);
+		} else if (channel.type !== "voice") {
+			console.error("Id não é de um canal de voz oooo bocó!");
+			return process.exit(1);
+		}
+
+		try {
+			await createBroadcast(client);
+			play(channel, client);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}
