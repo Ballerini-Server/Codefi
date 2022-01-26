f42d81c30aec5f90b229859748ffa51d8a73be5c
tauzxd <tauasanto101@gmail.com>
2022-01-26 13:57:58 -0300

    bug after half an hour fixed, stream was not updating

--- index.js
+++ index.js
@@ -15,30 +15,31 @@ if (!TOKEN) {
 
 let channel = null;
 let broadcast = null;
-let interval = null;
-let stream = ytdl(URL, { highWaterMark: 100 << 150 });
-
+let stream = ytdl(URL, { highWaterMark: 100 << 150,filter: 'audio'})
 client.on('ready', async() => {
-
-    client.user.setActivity(STATUS || "Radio lo-fi",{ type: 'LISTENING'})
+    
+    client.user.setActivity(STATUS || "Radio CODE-FI",{ type: 'LISTENING'})
     channel = client.channels.cache.get(CHANNELID) || await client.channels.fetch(CHANNELID);
 
     if (!channel || channel.type !== "voice") return console.error("canal de voz não existe")
 
     broadcast = client.voice.createBroadcast();
     stream.on('error', console.error);
-    broadcast.play(stream);
 
-    if (!interval) {
-        interval = setInterval(async () => await channel.leave(), 1800000)
-    }
     try {
         const connection = await channel.join();
+        broadcast.play(stream);
         connection.play(broadcast);
+        
+        setInterval(async ()=>{   
+                stream.destroy()
+                stream = ytdl(URL, { highWaterMark: 100 << 150, filter: 'audio'});
+                await broadcast.play(stream)
+        }, 1800000)
     } catch (error) {
         console.error(error);
     }
-    console.log("sucess")
+  
 });
 
 client.on('voiceStateUpdate', async userEvent => {
